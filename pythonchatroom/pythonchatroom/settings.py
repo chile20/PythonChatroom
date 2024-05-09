601e71944f04b25183e03c2f0afcd74c30c5adb6
Chi Le <cdle@cpp.edu>
2024-05-08 21:50:05 -0700

    prep for deployment

--- pythonchatroom/pythonchatroom/settings.py
+++ pythonchatroom/pythonchatroom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^lnen_*bfqf%!+^jl1c1yl81%_uf6xuap66y=a54en10ol4ml^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
