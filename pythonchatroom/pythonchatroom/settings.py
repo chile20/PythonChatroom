6221d341318447d0bb496fde0068ffab6c8887e3
Chi Le <cdle@cpp.edu>
2024-05-09 01:37:33 -0700

    debug on

--- pythonchatroom/pythonchatroom/settings.py
+++ pythonchatroom/pythonchatroom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^lnen_*bfqf%!+^jl1c1yl81%_uf6xuap66y=a54en10ol4ml^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'python-chat-019d01755ef6.herokuapp.com']
 
